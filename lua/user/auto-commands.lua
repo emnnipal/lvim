77b5be822073169faf5867e1f9fb916cb8dbd5d3
Emman Nipal <emnnipal@gmail.com>
2023-03-28 16:02:56 +0800

    chore: remove auto fix in eslint for better performance and battery usage

--- lua/user/auto-commands.lua
+++ lua/user/auto-commands.lua
@@ -1,11 +1,11 @@
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
-lvim.autocommands = {
-  {
-    "BufWritePre", -- see `:h autocmd-events`
-    {
-      -- this table is passed verbatim as `opts` to `nvim_create_autocmd`
-      pattern = { "*.ts", "*.tsx", "*.js", "*.jsx", }, -- see `:h autocmd-events`
-      command = "EslintFixAll",
-    }
-  },
-}
+-- lvim.autocommands = {
+--   {
+--     "BufWritePre", -- see `:h autocmd-events`
+--     {
+--       -- this table is passed verbatim as `opts` to `nvim_create_autocmd`
+--       pattern = { "*.ts", "*.tsx", "*.js", "*.jsx", }, -- see `:h autocmd-events`
+--       command = "EslintFixAll", -- auto fix eslint errors on save
+--     }
+--   },
+-- }
