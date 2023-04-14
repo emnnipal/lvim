0ecc6039da17b77c48c6dd867c2a691efce7310b
Emman Nipal <emnnipal@gmail.com>
2023-04-14 21:58:33 +0800

    chore: re-add auto fix with eslint because this is convenient and just consumes 3~5% of 1 core of cpu upon execution

--- lua/user/auto-commands.lua
+++ lua/user/auto-commands.lua
@@ -2,12 +2,12 @@
 
 -- Commented out because it's not that needed and it consumes a lot of cpu
 -- resources in large projects.
--- lvim.autocommands = {
---   {
---     "BufWritePre", -- see `:h autocmd-events`
---     {
---       pattern = { "*.ts", "*.tsx", "*.js", "*.jsx", },
---       command = "EslintFixAll", -- auto fix eslint errors on save
---     }
---   },
--- }
+lvim.autocommands = {
+  {
+    "BufWritePre", -- see `:h autocmd-events`
+    {
+      pattern = { "*.ts", "*.tsx", "*.js", "*.jsx", },
+      command = "EslintFixAll", -- auto fix eslint errors on save
+    }
+  },
+}
